e401c649359d9da16fa70cd052ba7aa1ce75434e
MORGANJASON <420morganjason@gmail.com>
2023-04-05 18:46:12 +0300

    created slides

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ function carousel() {
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  x[myIndex-1].style.display = "flex";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
 }
