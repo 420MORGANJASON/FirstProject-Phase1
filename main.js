931196e3ab5a14cb9e357331a8540e3aac10e5d8
MORGANJASON <420morganjason@gmail.com>
2023-04-07 00:49:30 +0300

    created feedback form

--- main.js
+++ main.js
@@ -5,32 +5,121 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-var myIndex = 0;
-carousel();
+// JavaScript code to hide text overlay on slide change
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+var textOverlays = document.getElementsByClassName("text-overlay");
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "flex";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+showSlides();
+
+function showSlides() {
+    // Hide all text overlays initially
+    for (var i = 0; i < textOverlays.length; i++) {
+        textOverlays[i].style.opacity = "0";
+    }
+
+    // Show current slide and corresponding text overlay
+    slides[slideIndex].style.display = "block";
+    textOverlays[slideIndex].style.opacity = "1";
+
+    // Increment slide index and set timeout for next slide
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
+const feedbackForm = document.querySelector('.feedback-container form');
+const resetButton = document.querySelector('#reset');
+const responseMessage = document.querySelector('.feedback-container .response');
+const savedFeedback = document.querySelector('#saved-feedback');
 
-const submitSuccess = document.querySelector('.submit-success')
-const submitError = document.querySelector('.submit-error')
+feedbackForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent form submission
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let myForm = document.getElementById("contact-form");
-  let formData = new FormData(myForm);
+  // get feedback and rating values
+  const feedbackValue = document.querySelector('#feedback').value;
+  const ratingValue = document.querySelector('#rating').value;
+
+  // save feedback value to savedFeedback element
+  savedFeedback.textContent = feedbackValue;
+
+  // simulate a response message
+  responseMessage.textContent = 'Thank you for your feedback!';
+
+  // clear form fields
+  feedbackForm.reset();
+});
+
+resetButton.addEventListener('click', () => {
+  // clear saved feedback and response message
+  savedFeedback.textContent = '';
+  responseMessage.textContent = '';
+
+  // reset form fields
+  feedbackForm.reset();
+});
+
+// Add click event listener to each book button
+const bookButton = document.querySelector('.book-btn');
+const seatsCount = document.querySelector('.seats');
+
+let availableSeats = parseInt(seatsCount.innerText);
+
+bookButton.addEventListener('click', function() {
+  if (availableSeats > 0) {
+    availableSeats--;
+    seatsCount.innerText = availableSeats.toString();
+    alert('Seat booked successfully!');
+  } else {
+    alert('Oops sorry, no more seats available.');
+  }
+});
+const bookButtons = document.querySelectorAll('.book-btn');
+bookButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const seats = button.previousElementSibling.querySelector('.seats');
+    const numSeats = parseInt(seats.innerText);
+    if (numSeats > 0) {
+      seats.innerText = numSeats - 1;
+      alert('Seat booked successfully!');
+  } else {
+    alert('Oops sorry, no more seats available.');
+  }
+    }
+ ) });
+// rating 
+ function submitRating() {
+  const ratingInput = document.getElementById("rating");
+  const rating = ratingInput.value;
+
+  if (rating >= 9) {
+    alert("Thank you for your feedback! We're glad you loved it!");
+  } else if (rating >= 5 && rating < 9) {
+    alert("Thank you for your feedback! We'll use it to make improvements.");
+  } else if (rating > 0 && rating < 5) {
+    alert("Thank you for your feedback! We're sorry we didn't meet your expectations.");
+  } else {
+    alert("Please select a rating before submitting.");
+  }
+}
+
+document.getElementById("rate-button").onclick = submitRating;
+
+
+
+// document
+//   .querySelector("form")
+//   .addEventListener("submit", handleSubmit);
+
+// const submitSuccess = document.querySelector('.submit-success')
+// const submitError = document.querySelector('.submit-error')
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   let myForm = document.getElementById("contact-form");
+//   let formData = new FormData(myForm);
 //   fetch("http://localhost:3000/vehicles", {
 //     method: "POST",
 //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -42,48 +131,48 @@ const handleSubmit = (e) => {
 // };
 
 
-fetch("http://localhost:3000/vehicles")
-  .then(response => response.json())
-  .then(data => {
-    // 2. Extract details of the first vehicle
-    const car = data.vehicles[0];
-    const { poster, number, capacity, title} = car;
-    document.getElementById('poster').src = poster;
-    document.getElementById('number').textContent = `${number}`;
-    document.getElementById('capacity').textContent = capacity;
-     document.getElementById('title').textContent = title;
-       // 4. Create vehicle list
-    const vehiclesList = document.getElementById('vehicles');
-    data.vehicles.forEach(car => {
-      const { poster, number, capacity, title } = car;
-      const li = document.createElement('li');
-      li.innerHTML = `<img src="${poster}" alt="${title}"><div><h4>${title}</h4><p>capacity: ${capacity}</p></div>`;
-      li.addEventListener('click', () => showVehicleDetails(id));
-      vehiclesList.appendChild(li);
-        });    // 5. Set up buy ticket button click event taking buyTicket as a callback
-    document.getElementById('book-vehicle').addEventListener('click', bookVehicle);
-    })
-    .catch(error => {
-    console.log(error);
-    });   
- // Function to update the mo details
-    function updateVehicleDetails(car) {
-      const { poster, title, capacity, tickets_sold } = movie;
-      availableTickets = capacity - tickets_sold;
-      document.getElementById('poster').src = poster;
-      document.getElementById('number').textContent = `${number}`;
-      document.getElementById('capacity').textContent = capacity;
-      document.getElementById('title').textContent = title;
-    }
-    // Function to show vehicle details when a vehicle is clicked
-    function showVehicleDetails(id) {
-      fetch("http://localhost:3000/vehicles")
-        .then(response => response.json())
-        .then(data => {
-          const car = data.vehicle.find(car => car.id === id);
-          if (car) {
-            updateVehicleDetails(car);
-          }
-        })
-      .catch(error => console.error(error));
-      }
+// fetch("http://localhost:3000/vehicles")
+//   .then(response => response.json())
+//   .then(data => {
+//     // 2. Extract details of the first vehicle
+//     const car = data.vehicles[0];
+//     const { poster, number, capacity, title} = car;
+//     document.getElementById('poster').src = poster;
+//     document.getElementById('number').textContent = `${number}`;
+//     document.getElementById('capacity').textContent = capacity;
+//      document.getElementById('title').textContent = title;
+//        // 4. Create vehicle list
+    // const vehiclesList = document.getElementById('vehicles');
+    // data.vehicles.forEach(car => {
+    //   const { poster, number, capacity, title } = car;
+    //   const li = document.createElement('li');
+    //   li.innerHTML = `<img src="${poster}" alt="${title}"><div><h4>${title}</h4><p>capacity: ${capacity}</p></div>`;
+    //   li.addEventListener('click', () => showVehicleDetails(id));
+    //   vehiclesList.appendChild(li);
+    //     });    // 5. Set up buy ticket button click event taking buyTicket as a callback
+    // document.getElementById('book-vehicle').addEventListener('click', bookVehicle);
+    // })
+    // .catch(error => {
+    // console.log(error);
+    // });   
+//  // Function to update the mo details
+//     function updateVehicleDetails(car) {
+//       const { poster, title, capacity, tickets_sold } = movie;
+//       availableTickets = capacity - tickets_sold;
+//       document.getElementById('poster').src = poster;
+//       document.getElementById('number').textContent = `${number}`;
+//       document.getElementById('capacity').textContent = capacity;
+//       document.getElementById('title').textContent = title;
+//     }
+//     // Function to show vehicle details when a vehicle is clicked
+//     function showVehicleDetails(id) {
+//       fetch("http://localhost:3000/vehicles")
+//         .then(response => response.json())
+//         .then(data => {
+//           const car = data.vehicle.find(car => car.id === id);
+//           if (car) {
+//             updateVehicleDetails(car);
+//           }
+//         })
+//       .catch(error => console.error(error));
+//       }
